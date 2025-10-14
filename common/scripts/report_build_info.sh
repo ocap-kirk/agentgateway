431f66b8ac698f86b1c8e019a0755fc9e120b6ac
Krisztian F <103492698+krisztianfekete@users.noreply.github.com>
2025-10-14 20:18:13 +0200

    make shebang more portable (#542)
    Signed-off-by: krisztianfekete <git@krisztianfekete.org>

--- common/scripts/report_build_info.sh
+++ common/scripts/report_build_info.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 if BUILD_GIT_REVISION=$(git rev-parse HEAD 2> /dev/null); then
   if [[ -z "${IGNORE_DIRTY_TREE}" ]] && [[ -n "$(git status --porcelain 2>/dev/null)" ]]; then
     BUILD_GIT_REVISION=${BUILD_GIT_REVISION}"-dirty"
